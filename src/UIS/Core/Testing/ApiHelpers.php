b9db2ef422801f1ff7cdb62c70da99bd6d5a9162
Martiros <martiros61@gmail.com>
2015-04-21 17:05:27 +0400

    Implement IntegrationTest to package.

--- src/UIS/Core/Testing/ApiHelpers.php
+++ src/UIS/Core/Testing/ApiHelpers.php
@@ -1,62 +1,52 @@
-<?php namespace UIS\Core\Testing;
-
-use GuzzleHttp\Client as HttpClient;
+<?php
+namespace UIS\Core\Testing;
 
 trait ApiHelpers
 {
+    protected $responseObject;
+
     /**
-     * @var HttpClient
+     * @param string $uri
+     * @param string $method
+     * @param array $params
+     * @return static
      */
-    protected $client = null;
-
-    public function apiRequest($url, $method = 'get', $params = [])
+    public function apiRequest($uri, $method = 'GET', $params = [])
     {
-        $response = $this->getClient()->$method(
-            $url,
-            $this->makeParams($method, $params)
-        );
-
-        $this->assertJson($response->getBody() . '', 'Invalid API JSON response.');
-        return $response->json();
+        $response = $this->call($method, $uri, $params)->getContent();
+        $this->assertJson($response, 'Invalid API JSON response.');
+        $this->responseObject = json_decode($response);
+        return $this;
     }
 
-    protected function makeParams($method, $params = [])
+    public function apiDump()
     {
-        if ($method != 'get') {
-            $params['_token'] = $this->getXsrfToken();
-        }
-        return [
-            'cookies' => true,
-            'body' => $params
-        ];
+        dd($this->responseObject);
+        return $this;
     }
 
-    protected function getClient()
+    public function assertApiStatusOk()
     {
-        if ($this->client === null) {
-            $this->client = new HttpClient();
-        }
-        return $this->client;
+        $this->assertApiStatus('OK');
     }
 
-    public function getXsrfToken()
+    public function assertApiStatusNotAuth()
     {
-        $result = $this->apiRequest(url('/api/core/xsrfRefresh'));
-        return $result['data']['token'];
+        $this->assertApiStatus('NOT_AUTH');
     }
 
-    public function assertApiValidResponse($response)
+    public function assertApiStatusInvalidData()
     {
-        $this->assertJson($response->getBody() . '', 'Invalid API JSON response.');
+        $this->assertApiStatus('INVALID_DATA');
     }
 
-    public function assertApiOK($result)
+    public function assertApiStatusNotFound()
     {
-        $this->assertEquals($result['status'], 'OK', 'Invalid status, must be "OK" ');
+        $this->assertApiStatus('NOT_FOUND');
     }
 
-    public function assertApiInvalidData($result)
+    public function assertApiStatus($status)
     {
-        $this->assertEquals('INVALID_DATA', $result['status'], 'Invalid status, must be "INVALID_DATA" ');
+        $this->assertEquals($status, $this->responseObject->status, print_r((array)$this->responseObject, true));
     }
 }
--- src/UIS/Core/Testing/TestCase.php
+++ src/UIS/Core/Testing/TestCase.php
@@ -1,8 +1,48 @@
-<?php namespace UIS\Core\Testing;
+<?php
+namespace UIS\Core\Testing;
 
-use TestCase as BaseTestCase;
+use Laracasts\Integrated\Extensions\Laravel as IntegrationTest;
+use Faker\Factory as Faker;
+use BadMethodCallException;
+use Mockery;
+use Mockery\MockInterface;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends IntegrationTest
 {
+    protected $faker;
 
+    public function __construct()
+    {
+        $this->faker = Faker::create();
+    }
+
+    public function getUserForAuth()
+    {
+        throw new BadMethodCallException("getUserForAuth method not implemented.");
+    }
+
+    public function login()
+    {
+        $this->be($this->getUserForAuth());
+        return $this;
+    }
+
+    public function token()
+    {
+        $this->startSession();
+        return csrf_token();
+    }
+
+    /**
+     * @return \Mockery\MockInterface
+     */
+    public function mock($class)
+    {
+        $obj = $this->app->make($class);
+        if (empty($obj) || !($obj instanceof MockInterface)) {
+            $obj = Mockery::mock($class);
+            $this->app->instance($class, $obj);
+        }
+        return $obj;
+    }
 }
