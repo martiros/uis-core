2bedd48947112d624ada209ab81c0a0ee681aa74
= <martiros61@gmail.com>
2015-05-05 19:59:37 +0400

    Add Upload File Func. For Testing

--- src/UIS/Core/Testing/ApiHelpers.php
+++ src/UIS/Core/Testing/ApiHelpers.php
@@ -1,19 +1,32 @@
 <?php
 namespace UIS\Core\Testing;
 
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Filesystem\Filesystem;
+
 trait ApiHelpers
 {
     protected $responseObject;
 
     /**
+     * @var \Symfony\Component\Filesystem\Filesystem
+     */
+    protected $fs;
+
+    /**
      * @param string $uri
      * @param string $method
      * @param array $params
+     * @param array $cookies
+     * @param array $files
+     * @param array $server
+     * @param null $content
      * @return static
      */
-    public function apiRequest($uri, $method = 'GET', $params = [])
+    public function apiRequest($uri, $method = 'GET', $params = [], $cookies = [], $files = [], $server = [], $content = null)
     {
-        $response = $this->call($method, $uri, $params)->getContent();
+        $response = $this->call($method, $uri, $params, $cookies, $files, $server, $content)->getContent();
         $this->assertJson($response, 'Invalid API JSON response.');
         $this->responseObject = json_decode($response);
         return $this;
@@ -48,5 +61,33 @@ trait ApiHelpers
     public function assertApiStatus($status)
     {
         $this->assertEquals($status, $this->responseObject->status, print_r((array)$this->responseObject, true));
+    }
+
+    protected function uploadFile($filePath, $fileName = null, $error = 0)
+    {
+        $file = new File($filePath);
+        $fileName = $fileName ?: $file->getFilename();
+
+        $tempFilePath = storage_path('/app/temp_file_for_test_upload');
+        $this->getFilesystem()->copy($file->getRealPath(), $tempFilePath, true);
+
+        $file = new UploadedFile($tempFilePath, $fileName, $file->getMimeType(), $file->getSize(), $error, true) ;
+        $this->apiRequest(
+            '/api/media/fileUploader/upload',
+            'POST',
+            [ '_token' => $this->token() ],
+            [],
+            [ 'file' => $file ]
+        );
+        $this->assertApiStatus('OK');
+        return $this->responseObject->data->file_id;
+    }
+
+    public function getFilesystem()
+    {
+        if ($this->fs === null) {
+            $this->fs = new Filesystem();
+        }
+        return $this->fs;
     }
 }
